4d1756fd1d8ea62d4d1d0fe41e7fa9900c166f54
Samandar <samandarahadjonov@gmail.com>
2022-11-29 09:37:54 +0500

    Done: Draw RoundedRectangle

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:custom_paint_sample/preparing/line.dart';
 import 'package:custom_paint_sample/preparing/rectangle.dart';
+import 'package:custom_paint_sample/preparing/rounded_rectangle.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: DrawRectanglePreparing(),
+      home: RoundedRectanglePreparing(),
     );
   }
 }
---
+++ lib/preparing/rounded_rectangle.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class RoundedRectanglePreparing extends StatelessWidget {
+  const RoundedRectanglePreparing({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      body: Center(
+        child: Container(
+          color: Colors.white,
+          width: 300,
+          height: 300,
+          child: CustomPaint(
+            painter: RoundedRectDraw(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class RoundedRectDraw extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..strokeWidth = 10
+      ..color = Colors.blue
+      ..style = PaintingStyle.stroke;
+
+    const a = Offset(100, 100);
+    const b = Offset(230, 230);
+    const radius = Radius.circular(60);
+    final rect = Rect.fromPoints(a, b);
+
+    canvas.drawRRect(RRect.fromRectAndRadius(rect, radius), paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
+}
